3f93baf705829a445b4640e6ac38df20b002a83d
Robert Oprescu <robert.oprescu_ext@sonepar.com>
2025-10-24 14:50:27 +0300

    setup hot reload frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000, // Check for changes every second
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ frontend/src/app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-50">
+
+    </div>
+  );
+}
